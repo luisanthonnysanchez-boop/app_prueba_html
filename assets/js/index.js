742bd538998ecf8f8dd019091500ef3f691c41e4
luisanthonysanchez-boop <luisanthonnysanchez@gmail.com>
2026-06-11 02:20:04 -0500

    Agregando archivos js

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+const personas = {
+    nombre: "Juan",
+    apellido: "Perez",
+    edad: 30
+}
+
+console.log(personas.nombre);
+console.log(personas.apellido);
+console.log(personas.edad);
+console.log(personas);
